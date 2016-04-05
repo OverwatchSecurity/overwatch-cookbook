d8a7b8f92c46c32dee414fc488bf45dfa69c46ad
trotttrotttrott <trott@odaacabeef.com>
2016-04-05 00:05:45 -0400

    Version 0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# v0.1.1 (2016-04-05)
+* Fix resource name collision.
+
+# v0.1.0 (2016-04-05)
+* `overwatch_install` custom resource.
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'trott@citrusbyte.com'
 license          'Apache 2.0'
 description      'Installs/Configures overwatch'
 long_description 'Installs/Configures overwatch'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'compat_resource', '~> 12.8.0'
 
